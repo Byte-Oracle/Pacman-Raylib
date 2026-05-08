77e6964a858fcbc62a593b3c41ff9deead77d702
Kevin Stoffan <kevin@Kevins-MacBook-Pro.local>
2026-05-07 20:26:15 -0600

    Laid Groundwork I have no clue what I am doing so this is probably abhorent code for how simple it is.

--- src/main.c
+++ src/main.c
@@ -1,54 +1,37 @@
-/*
-Raylib example file.
-This is an example main file for a simple raylib project.
-Use this as a starting point or replace it with your code.
-
-by Jeffery Myers is marked with CC0 1.0. To view a copy of this license, visit https://creativecommons.org/publicdomain/zero/1.0/
-
-*/
-
 #include "raylib.h"
+#include "player.h"
 
-#include "resource_dir.h"	// utility header for SearchAndSetResourceDir
+Vector2 playerInput = {0, 0};
+float movementSpeed = 1.0;
 
-int main ()
-{
-	// Tell the window to use vsync and work on high DPI displays
-	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
+void DrawDebug(void);
+void PlayerMove(void);
 
-	// Create the window and OpenGL context
-	InitWindow(800, 600, "Hello Raylib");
-
-	// Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
-	SearchAndSetResourceDir("resources");
-
-	// Load a texture from the resources directory
-	Texture wabbit = LoadTexture("wabbit_alpha.png");
+int main(void){
+	bool showDebug = true;
+	bool playerEnabled = true;
 	
-	// game loop
-	while (!WindowShouldClose())		// run the loop until the user presses ESCAPE or presses the Close button on the window
-	{
-		// drawing
-		BeginDrawing();
+	//Double the native Pacman resolution because modern displays duh
+	const Vector2 screenSize = {448, 576};
 
-		// Setup the back buffer for drawing (clear color and depth buffers)
-		ClearBackground(BLACK);
-
-		// draw some text using the default font
-		DrawText("Hello Raylib", 200,200,20,WHITE);
-
-		// draw our texture to the screen
-		DrawTexture(wabbit, 400, 200, WHITE);
+	InitWindow(screenSize.x, screenSize.y, "PacClone");
+	SetTargetFPS(60);
+	
+	while(!WindowShouldClose()){
+		if(playerEnabled){
+			updatePlayer();
+		}
 		
-		// end the frame and get ready for the next one  (display frame, poll input, etc...)
+		BeginDrawing();
+			if(showDebug){DrawDebug();}	
 		EndDrawing();
 	}
 
-	// cleanup
-	// unload our texture so it can be cleaned up
-	UnloadTexture(wabbit);
-
-	// destroy the window and cleanup the OpenGL context
 	CloseWindow();
 	return 0;
+}
+
+void DrawDebug(){
+	ClearBackground(BLACK);
+	DrawText(TextFormat("PX: %.0f PY: %.0f", playerInput.x, playerInput.y), 0, 0, 10, LIGHTGRAY);
 }
